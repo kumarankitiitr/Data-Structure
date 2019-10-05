375d8609c7bdaf2592a4eeb0b1f9efaa348077ef
kumarankitiitr <47109085+kumarankitiitr@users.noreply.github.com>
2019-10-05 21:57:38 +0530

    produce a sorted list by merging two sorted list
    this is a function that takes heads of the lists as arguments. and return a new head of the merged sorted linked list

---
+++ mergesortedlist.py
@@ -0,0 +1,30 @@
+def mergeLists(head1, head2):
+    p = head1
+    q = head2
+    if p== None:
+        return q
+    if q== None:
+        return p
+    if(p.data <= q.data):
+        s = p
+        p = s.next
+    else:
+        s = q
+        q = s.next
+    new_head = s
+
+    while(p != None and q != None):
+        if(p.data <= q.data):
+            s.next = p
+            s = p
+            p = s.next
+        else:
+            s.next = q
+            s = q
+            q = s.next
+    if p== None:
+        s.next = q
+    if q== None:
+        s.next = p
+    
+    return new_head
