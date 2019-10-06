d350a46adea64ac6a5dcf16ca47ba6382616babe
kumarankitiitr <47109085+kumarankitiitr@users.noreply.github.com>
2019-10-07 02:31:31 +0530

    a class for queue with. arr or list implementation.
    Class with all basic functions

---
+++ arrimplement.py
@@ -0,0 +1,25 @@
+class queue:
+    def __init__(self):
+        self.items = []
+    def IsEmpty(self):
+        return self.items == []
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    def dequeue(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+    def printqueque(self):
+        for i in self.items:
+            print(i, end = ' ')
+        print()
+
+q = queue()
+print(q.size()) # 0
+q.enqueue(8)    # 8
+q.enqueue(9)    # 9 8
+q.enqueue(4)    # 4 9 8
+q.enqueue(3)    # 3 4 9 8 
+q.printqueque() # 3 4 9 8
+q.dequeue()     # dequeue 8
+q.printqueque() # 3 4 9
